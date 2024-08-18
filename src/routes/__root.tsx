85c16193800f1dde8eeaddac3df11cd92e4947c2
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-08-18 14:13:15 +1200

    devtools only for dev

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,24 +1,30 @@
+import React from "react";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+const TanStackRouterDevtools = React.lazy(
+	() => import("./-components/router-devtools")
+);
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <div className="w-screen">
-        <nav className="flex justify-center items-center sticky p-2 gap-4 bg-sky-500 text-white">
-          <Link to="/" className="[&.active]:font-bold">
-            Home
-          </Link>{" "}
-          <Link to="/about" className="[&.active]:font-bold">
-            About
-          </Link>
-          <Link to="/users" className="[&.active]:font-bold">
-            Users
-          </Link>
-        </nav>{" "}
-      </div>
-      <Outlet />
-      <TanStackRouterDevtools />
-    </>
-  ),
+	component: () => (
+		<>
+			<div className='w-screen'>
+				<nav className='flex justify-center items-center sticky p-2 gap-4 bg-sky-500 text-white'>
+					<Link to='/' className='[&.active]:font-bold'>
+						Home
+					</Link>{" "}
+					<Link to='/about' className='[&.active]:font-bold'>
+						About
+					</Link>
+					<Link to='/users' className='[&.active]:font-bold'>
+						Users
+					</Link>
+				</nav>{" "}
+			</div>
+			<Outlet />
+			<React.Suspense fallback={null}>
+				<TanStackRouterDevtools />
+			</React.Suspense>
+		</>
+	),
 });
