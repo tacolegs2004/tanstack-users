08aebad3f65eb169065c1d7527d00b04125deeee
taco boi <tylerchall16@gmail.com>
2024-08-23 20:08:16 -0500

    restyled site

--- src/routes/users/$userId.tsx
+++ src/routes/users/$userId.tsx
@@ -3,58 +3,66 @@ import { createFileRoute, notFound } from "@tanstack/react-router";
 import { IUsers } from "../../types";
 
 const getUser = async (userId: string): Promise<IUsers> => {
-	return await fetch(
-		`https://jsonplaceholder.typicode.com/users/${userId}`
-	).then((resp) => {
-		return resp.ok ? resp.json() : Promise.reject(resp.statusText);
-	});
+  return await fetch(
+    `https://jsonplaceholder.typicode.com/users/${userId}`,
+  ).then((resp) => {
+    return resp.ok ? resp.json() : Promise.reject(resp.statusText);
+  });
 };
 
 const userOptions = (userId: string) =>
-	queryOptions({
-		queryKey: ["users", userId],
-		queryFn: () => getUser(userId),
-	});
+  queryOptions({
+    queryKey: ["userId", userId],
+    queryFn: () => getUser(userId),
+  });
 
 export const Route = createFileRoute("/users/$userId")({
-	component: User,
-	beforeLoad: ({ params }) => {
-		return {
-			getUserOptions: userOptions(params.userId),
-		};
-	},
-	loader: async ({ context }) => {
-		try {
-			await context.queryClient.ensureQueryData(context.getUserOptions);
-		} catch (_) {
-			throw notFound();
-		}
-	},
-	pendingComponent: () => (
-		<div className='grid place-items-center'>
-			<p>Customize this /users/$userId loading...</p>
-		</div>
-	),
-	notFoundComponent: NotFoundComp,
+  component: User,
+  beforeLoad: ({ params }) => {
+    return {
+      getUserOptions: userOptions(params.userId),
+    };
+  },
+  loader: async ({ context }) => {
+    try {
+      await context.queryClient.ensureQueryData(context.getUserOptions);
+    } catch (_) {
+      throw notFound();
+    }
+  },
+  pendingComponent: () => (
+    <div className="flex flex-col items-center justify-center">
+      <span className="flex h-[360px] w-screen flex-col items-center justify-end bg-gradient-to-r from-slate-500 to-slate-400 text-5xl font-bold text-white">
+        <p>Loading...</p>
+      </span>
+    </div>
+  ),
+  notFoundComponent: NotFoundComp,
 });
 
 function NotFoundComp() {
-	const id = Route.useParams({ select: (s) => s.userId });
+  const id = Route.useParams({ select: (s) => s.userId });
 
-	return `User with id ${id} not found, you can implement your own 404 page or UI to handle resetting this page`;
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <section className="flex h-[50%] w-screen flex-col items-center justify-start bg-gradient-to-r from-slate-500 to-slate-400 text-5xl font-bold text-white">
+        <h1 className="text-3xl font-bold">Data failed to fetch.</h1>
+      </section>
+    </div>
+  );
 }
 
 function User() {
-	const getUserOptions = Route.useRouteContext({
-		select: (s) => s.getUserOptions,
-	});
-	const userQuery = useSuspenseQuery(getUserOptions);
+  const getUserOptions = Route.useRouteContext({
+    select: (s) => s.getUserOptions,
+  });
+  const userQuery = useSuspenseQuery(getUserOptions);
 
-	return (
-		<div className='flex flex-col items-center justify-center'>
-			<span className='flex flex-col justify-start items-center w-screen h-[50%] bg-gradient-to-r  from-slate-500 to-slate-400 text-white text-5xl font-bold'>
-				<h1 className='text-3xl font-bold'>{userQuery.data?.name}</h1>
-			</span>
-		</div>
-	);
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <span className="flex h-[360px] w-screen flex-col items-center justify-end bg-gradient-to-r from-slate-500 to-slate-400 text-5xl font-bold text-white">
+        <h1 className="text-3xl font-bold">{userQuery.data?.name}</h1>
+      </span>
+    </div>
+  );
 }
